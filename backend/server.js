93a0ed743e87e1dcc2e6cfb9464f44bdfbf8b54b
Pradumby <anupy08115@gmail.com>
2026-03-10 23:26:54 +0530

    feat: add MongoDB connection using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const app = require("./src/app");
+require("dotenv").config();
 
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
+    await connectDB();
+
     app.listen(PORT, () => {
       console.log("Server is running on:" + PORT);
     });
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 app.use("/", (req, res) => {
   res.json("NoteHive API running");
